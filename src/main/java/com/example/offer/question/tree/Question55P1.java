55ebf5b560a91f82a3a531ed37d69868facfd23d
lihaibin <lihaibin928383374@gmail.com>
2021-05-25 12:26:23 +0800

    面试题55-1：二叉树的深度（层序遍历解法）

--- src/main/java/com/example/offer/question/tree/Question55P1.java
+++ src/main/java/com/example/offer/question/tree/Question55P1.java
@@ -4,13 +4,23 @@ import com.example.offer.question.linkedlist.Question24;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @Description: 剑指 Offer 55 - I. 二叉树的深度
- *      1.说明：输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
- *      2.示例：
- *      3.参考：https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof/
  * @Author: lhb
  * @Date: 2021/5/21 15:56
+ *
+ * 1.说明：输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，最长路径的长度为树的深度。
+ * 2.示例：
+ * 3.参考：https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof/
+ * 4.笔记：
+ *      1.树的遍历一般分为两种：
+ *          * DFS：Depth First Search，深度优先搜索
+ *          * BFS：Breadth First Search，广度优先搜索
+ *      2.常见的DFS：先序遍历、中序遍历、后序遍历
+ *      3.常见的BFS：层序遍历
  */
 
 public class Question55P1 {
@@ -60,5 +70,41 @@ public class Question55P1 {
         int rightDepth = solution1(root.right);
         // 返回当前节点左右子树中较大的深度，再加上自身，就是这棵树的最大深度
         return (leftDepth >= rightDepth ? leftDepth : rightDepth) + 1;
+    }
+
+    /**
+     * 解法2：
+     *      思路：层序遍历
+     * @param root
+     * @return
+     */
+    public static int solution2(TreeNode root) {
+        // 树的根节点为null，直接返回0
+        if (root == null) {
+            return 0;
+        }
+
+        // 记录树的深度
+        int depth = 0;
+        // 存储当前层的节点，初始化存储根节点
+        Queue<TreeNode> currentLayer = new LinkedList<>();
+        currentLayer.add(root);
+
+        while (!currentLayer.isEmpty()) {
+            // 临时存储下一层的节点
+            Queue<TreeNode> nextLayer = new LinkedList<>();
+            for (TreeNode node : currentLayer) {
+                if (node.left != null) {
+                    nextLayer.add(node.left);
+                }
+                if (node.right != null) {
+                    nextLayer.add(node.right);
+                }
+            }
+            currentLayer = nextLayer;
+            depth++;
+        }
+
+        return depth;
     }
 }
--- src/test/java/com/example/offer/question/tree/Question55P1Test.java
+++ src/test/java/com/example/offer/question/tree/Question55P1Test.java
@@ -41,4 +41,32 @@ class Question55P1Test {
         int result = Question55P1.solution1(root);
         log.info("output = {}", result);
     }
+
+    @Test
+    void solution2() {
+        // 初始化一棵二叉树
+        //       5
+        //      / \
+        //     8   4
+        //    / \
+        //   6   10
+        //  /
+        // 2
+        Question55P1.TreeNode root = new Question55P1.TreeNode(5);
+        Question55P1.TreeNode node1 = new Question55P1.TreeNode(8);
+        Question55P1.TreeNode node2 = new Question55P1.TreeNode(4);
+        Question55P1.TreeNode node3 = new Question55P1.TreeNode(6);
+        Question55P1.TreeNode node4 = new Question55P1.TreeNode(10);
+        Question55P1.TreeNode node5 = new Question55P1.TreeNode(2);
+        root.setLeft(node1).setRight(node2);
+        node1.setLeft(node3).setRight(node4);
+        node3.setLeft(node5);
+
+        // 输入
+        log.info("input = {}", root);
+
+        // 输出
+        int result = Question55P1.solution2(root);
+        log.info("output = {}", result);
+    }
 }
