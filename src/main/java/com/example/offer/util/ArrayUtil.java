e7d6369736f5bd8d75563c527dbec932e303d744
lihaibin <lihaibin928383374@gmail.com>
2021-05-31 21:12:34 +0800

    fix(数组): 面试题03：数组中重复的数字
    1.修改：ArrayUtil生成的数组，使之符合"面试题03"的要求

--- src/main/java/com/example/offer/util/ArrayUtil.java
+++ src/main/java/com/example/offer/util/ArrayUtil.java
@@ -22,7 +22,7 @@ public class ArrayUtil {
      * @return
      */
     public static int[] createNoRepeatedArray() {
-        int[] nums = new int[] {2, 3, 4, 5, 6, 7, 8};
+        int[] nums = new int[] {0, 1, 2, 3, 4, 5, 6};
         return nums;
     }
 
