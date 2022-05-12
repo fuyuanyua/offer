2bd49cb28442d577824fb59c2c77f2ff477299a3
lihaibin <lihaibin928383374@gmail.com>
2022-05-12 15:19:41 +0800

    面试题31

--- src/main/java/com/example/offer/question/stack/medium/Question31.java
+++ src/main/java/com/example/offer/question/stack/medium/Question31.java
@@ -11,14 +11,14 @@ public class Question31 {
     public static boolean solution1(int[] pushed, int[] popped) {
         // 辅助栈
         Stack<Integer> stack = new Stack<>();
-        // 记录popped下一个要弹出的索引
+        // 记录popped下一个要弹出的元素的索引
         int index = 0;
 
         // 按pushed顺序，把元素压入辅助栈
         for (int i = 0; i < pushed.length; i++) {
             int item = pushed[i];
             stack.push(item);
-            // 对比辅助栈栈顶元素与popped当前元素，相等则弹出辅助栈栈顶元素，并把popped索引位后移
+            // 对比辅助栈栈顶元素与popped索引指向元素，相等则弹出辅助栈栈顶元素，并把popped索引位后移
             // 循环执行
             while (!stack.isEmpty() && stack.peek() == popped[index]) {
                 stack.pop();
