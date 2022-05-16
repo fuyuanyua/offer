4ad749e18ea537388d9a4e12c1105ffc4cf8a301
lihaibin <lihaibin928383374@gmail.com>
2022-05-16 22:56:04 +0800

    链表（双指针技巧）：环形链表 II

---
+++ src/main/java/com/example/offer/milestone/linkedList/Question142.java
@@ -0,0 +1,55 @@
+package com.example.offer.milestone.linkedList;
+
+import com.example.offer.datastructure.ListNode;
+
+/**
+ * @Author: lihaibin
+ * @Date: 2022/5/16 22:23
+ * @Description: 环形链表 II：https://leetcode.cn/problems/linked-list-cycle-ii/
+ *
+ * 笔记：
+ *      相比较于Question141：环形链表，此题要求找出环形链表的入口
+ *      1.设置slow和fast两个指针，slow一次走一步，fast一次走两步，先找到相遇的节点
+ *      2.当slow和fast相遇后，将其中一个指针归位（重新指向head），
+ *      3.然后slow和fast都再以每次一步的步伐继续向后遍历，再次相遇的节点就是环形链表入口
+ */
+
+public class Question142 {
+
+    public static ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        // 慢指针，每次走一步，初始指向链表头节点
+        ListNode slow = head;
+        // 快指针，每次走两步，初始指向链表头节点
+        ListNode fast = head;
+
+        // fast遍历链表
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            // 找到了相遇节点，此时slow和fast都指向相遇节点
+            if (slow == fast) {
+                break;
+            }
+        }
+
+        // 若无环，返回null
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+
+        // 若有环
+        // slow重新指向head，然后继续遍历，slow和fast每次都同时走一步
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        // 找到环形链表入口，返回slow和fast都可以，都指向环形链表入口节点
+        return slow;
+    }
+}
---
+++ src/test/java/com/example/offer/milestone/linkedList/Question142Test.java
@@ -0,0 +1,26 @@
+package com.example.offer.milestone.linkedList;
+
+import com.example.offer.datastructure.ListNode;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class Question142Test {
+
+    @Test
+    void detectCycle() {
+        // 初始化一个环形链表
+        ListNode head = new ListNode(0);
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        head.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node1;
+
+        // 结果 = 1，环形链表入口是值为1的那个节点
+        ListNode result = Question142.detectCycle(head);
+        log.info("{}", result.value);
+    }
+}
